f59d07c781650ea389993ef35849b7b3a0f5102e
luz211 <zlt1213@gmail.com>
2019-06-12 11:04:00 +0800

    ch3p3 done

--- CH3_Gaussian_FIlters/scripts/ch3p1.py
+++ CH3_Gaussian_FIlters/scripts/ch3p1.py
@@ -1,29 +1,11 @@
+#====================================
+# This is the native python script for problem ch3p1
+#====================================
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from matplotlib.patches import Ellipse
-
-# NUM = 250
-#
-# ells = [Ellipse(xy=rnd.rand(2)*10, width=rnd.rand(), height=rnd.rand(), angle=rnd.rand()*360)
-#         for i in range(NUM)]
-#
-# fig = plt.figure(0)
-# ax = fig.add_subplot(111, aspect='equal')
-# for e in ells:
-#     ax.add_artist(e)
-#     e.set_clip_box(ax.bbox)
-#     e.set_alpha(rnd.rand())
-#     e.set_facecolor(rnd.rand(3))
-#
-# ax.set_xlim(0, 10)
-# ax.set_ylim(0, 10)
-#
-# plt.show()
-
-
-
-
 
 # define the matrix
 A = np.array([[1.0,1.0], [0.0, 1.0]])
